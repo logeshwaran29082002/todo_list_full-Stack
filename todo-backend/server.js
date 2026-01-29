6b896cec863111f6c650de5831d0f12bd4cc4ca9
Logeshwaran <logurashitha2002@gmail.com>
2026-01-29 11:36:17 +0530

    changes

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -96,8 +96,9 @@ app.delete("/todos/:id", async (req, res) => {
   }
 });
 
-//Start the server
-const port = 3000;
-app.listen(port, () => {
-  console.log("Server is listening on port" + port);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
+
